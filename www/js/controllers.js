ed30e74b9b4e9e10c1f7004fa7c8f89d9dae3fba
ghiridhar2712 <ghiridhar.pixolo@gmail.com>
2015-05-18 13:08:58 +0530

    kk

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,45 +1,93 @@
+var a;
 var cont = angular.module('controllers', [])
-	.factory('MyDatabase', function ($location) {
+    .factory('MyDatabase', function ($location) {
 
-		//WRITE DATABASE QUERIES HERE
-		return {
+        //WRITE DATABASE QUERIES HERE
+        return {
 
-			getusername: function () {
-				console.log("funstion called");
-				db.transaction(function (tx) {
-					tx.executeSql("SELECT `username` FROM `users` WHERE `id`= '1'", [], function (tx, results) {
-						user = results.rows.item(0);
-						console.log(user);
-						//console.log(cont.controller('questionsCtrl')().done());
-						//console.log(angular.element(document.getElementById('questionsCtrl')));
-						//console.log(angular.element(document.getElementById('questionsCtrl')).scope());
-					}, null);
-				})
-			},
-		}
-	})
-	.controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
-		// Form data for the login modal
-		$scope.loginData = {};
+            getusername: function () {
+                console.log("funstion called");
+                db.transaction(function (tx) {
+                    tx.executeSql("SELECT `username` FROM `users` WHERE `id`= '1'", [], function (tx, results) {
+                        user = results.rows.item(0);
+                        console.log(user);
+                        //console.log(cont.controller('questionsCtrl')().done());
+                        //console.log(angular.element(document.getElementById('questionsCtrl')));
+                        //console.log(angular.element(document.getElementById('questionsCtrl')).scope());
+                    }, null);
+                })
+            },
+        }
+    })
 
-	})
+    .controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
+        // Form data for the login modal
+        $scope.loginData = {};
+
+    })
 
 .controller('termsCtrl', function ($scope) {
 
 })
 
 .controller('loginCtrl', function ($scope) {
-	console.log("hi");
+    console.log("hi");
+    
+    $scope.forgetpassword=function(){
+        
+    
+        console.log("yo");
+    };
 })
 
-.controller('signupCtrl', function ($scope) {
-    $scope.user={};
-	$scope.change = function () {
-    
-    console.log($scope.user);
-    
-    
-    }
+.controller('signupCtrl', function ($scope,$http) {
+    $scope.user = [{}];
+       
+    $scope.change = function () {
+
+        console.log($scope.user);
+        $scope.namerequired = '';
+        $scope.genderrequired = '';
+        $scope.emailrequired = '';
+        $scope.passwordrequired = '';
+        $scope.contactrequired='';
+
+        if (!$scope.user.name) {
+            $scope.namerequired = 'Name Required !';
+        }
+        if(!$scope.user.gender){
+        $scope.genderrequired='Gender Required !';
+        }
+
+    if (!$scope.user.email) {
+            $scope.emailrequired = 'Email Required !';
+        }
+
+         if (!$scope.user.password) {
+            $scope.passwordrequired = 'Password Required !';
+        }
+         if (!$scope.user.contact) {
+            $scope.contactrequired = 'Contact Required !';
+        }
+        else{
+         $http.get('js/mydatabase.js', $scope.user
+).success(function(data, status, headers, config) {
+  // Do something successful
+}).error(function(data, status, headers, config) {
+  // Handle error
+});
+        }
+        //$scope.reset();
+    };
+   // $scope.reset=function(){
+    //$scope.user=[];
+    })
+  /*  $http.get('js/mydatabase.js', $scope.user
+).success(function(data, status, headers, config) {
+  // Do something successful
+}).error(function(data, status, headers, config) {
+  // Handle error
+});*/
 
 
 
@@ -47,8 +95,10 @@ var cont = angular.module('controllers', [])
 
 
 
-})
-       /* $scope.addBook = function()*/
+
+
+
+/* $scope.addBook = function()*/
 
 /*db({
 
@@ -68,24 +118,24 @@ $scope.books.push(data);
 });
 
 }*/
-		/*console.log("jyoti");
-        $scope.user={};
-        $scope.user.name="";
-    console.log($scope.user.name);*/
-	//})
+/*console.log("jyoti");
+      $scope.user={};
+      $scope.user.name="";
+  console.log($scope.user.name);*/
+//})
 
 
 .controller('questionsCtrl', function ($scope, MyDatabase) {
-	$scope.done = function () {
-		console.log("done");
-	};
+    $scope.done = function () {
+        console.log("done");
+    };
 
-	db.transaction(function (tx) {
-		tx.executeSql("SELECT `username` FROM `users` WHERE `id`= '1'", [], function (tx, results) {
-			user = results.rows.item(0);
-			console.log(user);
-		}, null);
-	});
+    db.transaction(function (tx) {
+        tx.executeSql("SELECT `username` FROM `users` WHERE `id`= '1'", [], function (tx, results) {
+            user = results.rows.item(0);
+            console.log(user);
+        }, null);
+    });
 })
 
 .controller('answersCtrl', function ($scope) {
